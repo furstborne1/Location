96b1b559842bdb7d702769be45225e78c1e57d56
furstborne1 <34493768+furstborne1@users.noreply.github.com>
2020-04-05 11:42:22 -0400

    Initial Commit

---
+++ Location/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Location
+//
+//  Created by MARC on 4/5/20.
+//  Copyright © 2020 MARC. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Location/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Location
+//
+//  Created by MARC on 4/5/20.
+//  Copyright © 2020 MARC. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
